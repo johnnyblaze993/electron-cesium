d5194b7a4079b039cc34e2f86aad9a9dc6b738ec
alvarezjo <jonathan.alvarez@axientcorp.com>
2024-10-22 15:33:58 -0500

    electron setup

---
+++ electron/electron.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron';
+// import * as path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadURL('http://localhost:5555'); // URL of Vite dev server
+
+  win.on('closed', () => {
+    win.destroy();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
