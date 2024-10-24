c5e550ec4b0f9dd787fdc998951eae64577d858d
alvarezjo <jonathan.alvarez@axientcorp.com>
2024-10-24 09:43:16 -0500

    sperate params into constants

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
     window.electronAPI.openTestWindow();  // Access the exposed API
   };
 
+  const position = Cartesian3.fromDegrees(-75.59777, 40.03883, 10);  // Longitude, Latitude, Height
+  const pointGraphics = { pixelSize: 10, color: Color.RED };  // Red point
+
   return (
     <div style={{ height: '100vh', width: '100vw', position: 'relative' }}>
       <button onClick={openTestWindow} style={{ position: 'absolute', zIndex: 1000 }}>
@@ -17,8 +20,10 @@ function App() {
       <Viewer full>
         {/* Example entity: a red point on the globe */}
         <Entity
-          position={Cartesian3.fromDegrees(-75.59777, 40.03883)}  // Longitude, Latitude
-          point={{ pixelSize: 10, color: Color.RED }}  // Red point
+          position={position}  // Longitude, Latitude
+          name='Red point'
+          description={'Hello, world!'}
+          point={pointGraphics}  // Red point
         />
       </Viewer>
     </div>
