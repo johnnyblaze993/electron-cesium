33a0c0dfa4bb9f9c20705c9332f43754b7799d8e
alvarezjo <jonathan.alvarez@axientcorp.com>
2024-11-20 13:02:56 -0600

    language switcher take effect before opening new test window

--- electron/electron.ts
+++ electron/electron.ts
@@ -53,7 +53,7 @@ function createMainWindow() {
   });
 }
 
-function createTestWindow() {
+function createTestWindow(initialLanguage: string) {
   const testWindow = new BrowserWindow({
     width: 600,
     height: 400,
@@ -66,34 +66,30 @@ function createTestWindow() {
     },
   });
 
-    // Load content and pass language after loading
-    const initialLanguage = i18n.language; // Capture the initial language
-    testWindow.webContents.on('did-finish-load', () => {
-      testWindow.webContents.send('update-language', initialLanguage);
-    });
+  // Send the initial language to the test window as soon as it loads
+  testWindow.webContents.on("did-finish-load", () => {
+    testWindow.webContents.send("update-language", initialLanguage);
+  });
 
   if (app.isPackaged) {
-    // Production: Load `index.html` and navigate to `#/test`
-    testWindow.loadFile(path.join(app.getAppPath(), 'dist', 'index.html')).then(() => {
+    testWindow.loadFile(path.join(app.getAppPath(), "dist", "index.html")).then(() => {
       testWindow?.webContents.executeJavaScript(`window.location.hash = "/test";`);
     });
   } else {
-    // Development: Load the Vite dev server URL with `#/test`
-    testWindow.loadURL('http://localhost:5173/#/test');
+    testWindow.loadURL("http://localhost:5173/#/test");
   }
 
   testWindow.setMenuBarVisibility(false);
 
-  testWindow.on('closed', () => {
+  testWindow.on("closed", () => {
     testWindows = testWindows.filter((win) => win !== testWindow);
   });
 
   testWindows.push(testWindow); // Add the new test window to the array
 }
-
 // IPC listener for opening the test window
-ipcMain.on('open-test-window', () => {
-  createTestWindow();
+ipcMain.on('open-test-window', (_event, language: string) => {
+  createTestWindow(language); // Pass the current language to the test window
 });
 
 // IPC listener for sending coordinates to the main window
--- electron/preload.ts
+++ electron/preload.ts
@@ -3,7 +3,7 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  openTestWindow: () => ipcRenderer.send('open-test-window'),
+  openTestWindow: (language: string) => ipcRenderer.send("open-test-window", language),
   sendCoordinates: (coordinates: any) => ipcRenderer.send('send-coordinates', coordinates),
   onCoordinatesUpdate: (callback: (arg0: any) => void) => ipcRenderer.on('update-coordinates', (_event, data) => callback(data)),
   setLanguage: (language: string) => ipcRenderer.send('set-language', language),
