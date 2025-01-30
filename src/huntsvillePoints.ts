83bae5081e2e79669b9c6901bc9f3c1282061d2a
alvarezjo <jonathan.alvarez@axientcorp.com>
2025-01-30 13:12:36 -0600

    to dos  added as comments for moving spline

--- src/huntsvillePoints.ts
+++ src/huntsvillePoints.ts
@@ -1,5 +1,10 @@
 // huntsvillePoints.ts
 // huntsvillePoints.ts
+// TODO - Add velocity 
+    // - time slicing and animating lines
+    // - time stamp
+// states should be an array with values of an enum
+// if only 1 state active, w eneed an order or precidence. 
 export const huntsvillePoints = [
     { longitude: -86.5861, latitude: 34.7304, altitude: 250, visible: true, acquired: false },
     { longitude: -86.5855, latitude: 34.7312, altitude: 255, visible: true, acquired: true },
